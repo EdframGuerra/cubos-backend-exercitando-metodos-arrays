405a33cabeba8f9c572e5b5e61cac94413eb77d7
Edfram-Guerra <edfram.guerra@gmail.com>
2022-10-14 13:16:35 -0300

    Exercicio resolvido

--- 05/scripts.js
+++ 05/scripts.js
@@ -1,1 +1,28 @@
+
 const pacientes = ['José', 'Pedro', 'Maria', 'João', 'Ana', 'Bárbara', 'Joana'];
+
+let agendamento = "agendar";
+let atendimento = "atender";
+
+
+function atendimentoMedico(lista, nome) {
+    // if (tipoDeOperacao == "agendar") {
+    //     lista.push(nome);
+    // } else {
+    //     lista.shift();
+    // }
+    // console.log(lista.join(", "));
+
+    if (nome) {
+        lista.push(nome);
+    } else {
+        lista.shift();
+    }
+    console.log(lista.join(", "));
+}
+
+atendimentoMedico(pacientes)
+atendimentoMedico(pacientes)
+atendimentoMedico(pacientes)
+atendimentoMedico(pacientes, "Zé")
+atendimentoMedico(pacientes, "Yan")
